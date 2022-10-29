d59d1cc5049d3d598b4b363133dbce726138569a
charlesarmah1002 <116085774+charlesarmah1002@users.noreply.github.com>
2022-10-29 02:39:29 +0000

    Add files via upload

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -2,6 +2,20 @@ let menuButton = document.querySelector('.menu-toggle')
 let menuDrop = document.querySelector('.menu')
 
 menuButton.addEventListener('click', function () {
-    menuButton.classList.toggle('toggle-active')
-    menuDrop.classList.toggle('drop-active')
+    menuButton.classList.add('toggle-active')
+    menuDrop.classList.add('drop-active')
+})
+
+menuButton.addEventListener('click', function() {
+    if(menuButton.className = 'toggle-active'){
+        menubutt
+    }
+})
+
+let menuItem = document.querySelector('.menu-item')
+
+menuItem.addEventListener('click', function() {
+    menuButton.classList.remove('toggle-active')
+    menuDrop.classList.remove('drop-active')
+    window.alert('this button was clicked')
 })
